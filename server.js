784014244544f7740e6f66e734dd7eaa81ef7716
张鹏飞 <fei472625@foxmail.com>
2018-11-10 20:52:34 +0800

    隐藏不必要逻辑

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-var url = require('url');
 var http = require('http');
-var handle = require('./lib/handle');
 var router = require('./router/router');
 
 var map = router.parseConfig(router.getConfig());
 http.createServer(function(req, res) {
-  for (var i in map.url) {
-    if (url.parse(req.url, true).pathname === map.url[i]) {
-      eval("handle." + map.func[i] + "(req, res)");
-    }
-  }
+  router.router(req, res, map);
 }).listen(3000);
